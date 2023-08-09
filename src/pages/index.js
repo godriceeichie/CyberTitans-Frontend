744cee8608f132ab5ce98340029b4333adc7c41c
just4tunes <fortuneosuh04@gmail.com>
2023-08-09 16:34:16 +0100

    unfinshed FAQ

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export {default as Faq} from "./Faq";
+export {default as Signin} from "./Signin";
+export {default as Signup} from "./Signup";
+export { default as About} from "./About";
+export { default as Contact } from "./Contact"
+export { default as Home } from "./Home";
