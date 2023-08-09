290f033f3afd63e43cf682cb61e434aa1e1be949
Wutche <wutchechikaome75@gmail.com>
2023-08-09 14:13:13 +0100

    started userdashboard

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
+/** @format */
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Contact from "./pages/Contact";
+import UserDashboard from "./pages/UserDashboard";
 
 function App() {
-  return<>
-    <Router>
-        <Routes>
-
-          <Route path="/">
-            <Route index element={<Home/>}/>
-            <Route path="about" element={<About/>}/>
-            <Route path="signin" element={<Signin/>}/>
-            <Route path="signup" element={<Signup/>}/>
-            <Route path="contact" element={<Contact/>}/>
-          </Route>
-          
-        </Routes>
-    </Router>
-  </>
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route path="/">
+						<Route index element={<Home />} />
+						<Route path="about" element={<About />} />
+						<Route path="signin" element={<Signin />} />
+						<Route path="signup" element={<Signup />} />
+						<Route path="contact" element={<Contact />} />
+						<Route path="userdashboard" element={<UserDashboard />} />
+					</Route>
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
---
+++ src/pages/UserDashboard.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const UserDashboard = () => {
+	return (
+		<div className="userwrapper">
+			<h1>heloo</h1>
+		</div>
+	);
+};
+
+export default UserDashboard;
