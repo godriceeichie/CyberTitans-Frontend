b4de6eafbed060f154b71dd3dbe320534dbf9bde
just4tunes <fortuneosuh04@gmail.com>
2023-08-10 04:55:54 +0100

    finished FAQ page

---
+++ src/components/data.js
@@ -0,0 +1,31 @@
+export const data = [ 
+    {
+        question: "How can i pay for my order",
+        answer:
+        "Lorem ipsum dolor sit amet, consectetur."
+    },
+    {
+        question: "What is amazon pay",
+        answer:
+        "Lorem ipsum dolor sit amet, consectetur."
+    },
+    {
+        question: "I want to change my account information",
+        answer:
+        "Lorem ipsum dolor sit amet, consectetur."
+    },
+    {
+        question: "How long does delevery take",
+        answer:
+        "Lorem ipsum dolor sit amet, consectetur."
+    },
+    {
+        question: "How long does delevery take",
+        answer:
+        "Lorem ipsum dolor sit amet, consectetur."
+    },
+
+
+
+
+]
--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,99 +1,83 @@
 import React from 'react';
 import '../styles/Faq.css'
-import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbSeparator,
-    VStack,
-  } from '@chakra-ui/react'
+import { data } from '../components/data';
+import { Accordion } from '../components/Accordion';
+
+
 
 
 const Faq = () => {
+    
     return (
       <>
-      <section className='FAQ-container'>
-        <div className='FAQ-top'>
-            <VStack>
-                <h1>FAQ</h1>
-                <Breadcrumb border={'none'} pos={'static'} >
-                    <BreadcrumbItem font-size ={''}>
-                        <BreadcrumbLink ib href='#'>Home</BreadcrumbLink>
-                    </BreadcrumbItem>
+      <section className='hero1'>
+        <div className='hero-left'>
+            <img src='https://images.unsplash.com/photo-1479064312651-24524fb55c0e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=800'></img>
+        </div>
 
-                    <BreadcrumbItem isCurrentPage>
-                        <BreadcrumbLink href='#'>FAQ</BreadcrumbLink>
-                    </BreadcrumbItem>
-                </Breadcrumb>
-            </VStack>
+        <div className='hero-right'>
+            <div className='hero-right h1c'>
+            <span>Help center</span>
+            <h1>How can We help you</h1>
+            <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+
+            <div className='input-field'>
+                <input   type="text" id="helpInput" name="helpInput" placeholder="what can we do for you?"></input>
+            </div>
         </div>
       </section>
 
-
-
-      <section className='Questions'>
-        <div className='Questions-container1'>
-        <div className='Questions-top'>
-        <h1>Frequently Asked Questions</h1>
-        <span>We focu son economics and meeting you where you work its only a keystroke </span>
-        </div>
-        </div>
-
-        <div className='boxes'>
-            <div className='box1'>
-                <h1>Work better together</h1>
-                <span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque euismod velit eget eros volutpat, sit amet."</span>
+      <section className='hero2'>
+        <div className='hero2-right'>
+            <h1>GENERAL QUESTIONS</h1>
+            <div className='question-fields'>
+                {data.map((section,index)=>(
+                    <Accordion key={index}  section={section}/>
+                ))}
             </div>
         </div>
 
-        <div className='boxes'>
-            <div className='box1'>
-                <h1>Work better together</h1>
-                <span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque euismod velit eget eros volutpat, sit amet."</span>
+        <div className='hero2-left'>
+        <h1>GENERAL QUESTIONS</h1>
+            <div className='question-fields'>
+                {data.map((section,index)=>(
+                    <Accordion key={index}  section={section}/>
+                ))}
             </div>
-        </div>
-
-        <div className='boxes'>
-            <div className='box1'>
-                <h1>Work better together</h1>
-                <span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque euismod velit eget eros volutpat, sit amet."</span>
-            </div>
-        </div>
-        <div className='boxes'>
-            <div className='box1'>
-                <h1>Work better together</h1>
-                <span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque euismod velit eget eros volutpat, sit amet."</span>
-            </div>
-        </div>
-        <div className='boxes'>
-        <div className='box1'>
-    <h1>Work better together</h1>
-    <span>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque euismod velit eget eros volutpat, sit amet."</span>
-</div >
-        </div>
 
 
-        <section className='signup-container'>
-        <div className='signup-c1'>
-            <h1>Be the first to know </h1>
         </div>
-        <div className='signup-c2'>
-        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque euismod velit eget ero</span>
-        </div>
-
-        <div></div>
-        
-
-      </section>
       </section>
 
+      <section className='hero-3'>
+        <div className='hero-3 container1'>
+            <div className='c-box1'>
+                <div className='c-box1-left'>
+                </div>
+                <div className='c-box1-right'>
+                    <span>Ask in the forums</span>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+        </div>
 
+        <div className='hero-3 container2'>
+        <div className='c-box2'>
+             <div className='c-box2-left'>
+                </div>
+                <div className='c-box2-right'>
+                    <img src=''></img>
+                    <span>Visit our blog now</span>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+         </div>
+        </div>
 
-
-      
-      
+      </section>
       </>
     );
 };
+
 
 export default Faq;
