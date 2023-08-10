57f50415a0bc23957ee9f48dd2c2d68cc62e839e
Nwabuisi Obinna Divine <obinnadivine107@gmail.com>
2023-08-10 09:33:56 +0100

    product page started

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import '../styles/Product.css'
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <div>
+      <section className='productMain'>
+        <h1>
+          Our Product
+        </h1>
+        <div className='categories'>
+          
+        </div>
+      </section>
+    </div>
   )
 }
 
