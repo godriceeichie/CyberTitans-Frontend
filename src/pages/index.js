8f335d9feb956d0ec620ee9af0563c5324399f92
Nwabuisi Obinna Divine <obinnadivine107@gmail.com>
2023-08-10 16:47:57 +0100

    error fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ export { default as Order} from "./account/Order";
 export { default as Settings} from "./account/Settings";
 export { default as Wishlist} from "./account/Wishlist";
 export { default as UserDashboard} from "./UserDashboard";
-export { default as Shop } from "./Shop";
 export {default as Faq} from "./Faq";
 export {default as Signin} from "./Signin";
 export {default as Signup} from "./Signup";
