a1c59a481c0bf22d2fec42e2079640a02521d176
Wutche <wutchechikaome75@gmail.com>
2023-08-10 22:18:54 +0100

    did some bootstrping

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
-export { default as History} from "./userdashboard/History";
-export { default as Inbox} from "./userdashboard/Inbox";
-export { default as Order} from "./userdashboard/Order";
-export { default as Settings} from "./userdashboard/Settings";
-export { default as Wishlist} from "./userdashboard/Wishlist";
-export { default as UserDashboard} from "./UserDashboard";
-export {default as Faq} from "./Faq";
-export {default as Login} from "./Login";
-export {default as Signup} from "./Signup";
-export { default as About} from "./About";
-export { default as Contact } from "./Contact"
+/** @format */
+
+export { default as History } from "./userdashboard/History";
+export { default as Inbox } from "./userdashboard/Inbox";
+export { default as Order } from "./userdashboard/Order";
+export { default as Settings } from "./userdashboard/Settings";
+export { default as Wishlist } from "./userdashboard/Wishlist";
+export { default as Account } from "./userdashboard/Account";
+export { default as UserDashboard } from "./UserDashboard";
+export { default as Faq } from "./Faq";
+export { default as Login } from "./Login";
+export { default as Signup } from "./Signup";
+export { default as About } from "./About";
+export { default as Contact } from "./Contact";
 export { default as Home } from "./Home";
 export { default as Products } from "./Products";
