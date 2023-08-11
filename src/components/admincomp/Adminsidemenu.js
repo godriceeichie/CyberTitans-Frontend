3809ad261fef59ac7c2c5d7624e8b28f2154862c
Wutche <wutchechikaome75@gmail.com>
2023-08-11 04:42:48 +0100

    still on admin side menu

--- src/components/admincomp/Adminsidemenu.js
+++ src/components/admincomp/Adminsidemenu.js
@@ -2,11 +2,55 @@
 
 import React from "react";
 import Logo from "../Logo";
+import { NavLink } from "react-router-dom";
+import { RiDashboard2Fill } from "react-icons/ri";
+import { FaShopify } from "react-icons/fa";
+import { MdInventory } from "react-icons/md";
+import { VscFeedback } from "react-icons/vsc";
 const Adminsidemenu = () => {
 	return (
 		<div className="adminsidemenu">
-			<Logo />
-      na
+			<div className="navitems">
+				<Logo />
+
+				<div className="navlinks">
+					<NavLink>
+						{" "}
+						<span>
+							{" "}
+							<RiDashboard2Fill />{" "}
+						</span>
+						Overview
+					</NavLink>
+					<NavLink>
+						{" "}
+						<span>
+							{" "}
+							<FaShopify />{" "}
+						</span>
+						Orders
+					</NavLink>
+					<NavLink>
+						{" "}
+						<span>
+							<MdInventory />
+						</span>
+						Inventory
+					</NavLink>
+					<NavLink>
+						{" "}
+						<span>
+							<VscFeedback />
+						</span>
+						Reviews
+					</NavLink>
+				</div>
+			</div>
+
+			<div className="settings">
+				<NavLink>Settings</NavLink>
+				<NavLink>Log Out</NavLink>
+			</div>
 		</div>
 	);
 };
