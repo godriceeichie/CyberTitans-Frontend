59a993ee23969d157d2e4bbfb22cfd859f3e5c00
Nwabuisi Obinna Divine <obinnadivine107@gmail.com>
2023-08-11 09:52:04 +0100

    cart functionality 80% done

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../styles/Cart.css';
+import Swal from 'sweetalert2';
+import { useState } from 'react';
 
 const Cart = ({ cartItems, setCartItems }) => {
   const handleQuantityChange = (index, newQuantity) => {
@@ -24,6 +26,43 @@ const Cart = ({ cartItems, setCartItems }) => {
     setCartItems(updatedCartItems);
   };
 
+   const [showCheckoutModal, setShowCheckoutModal] = useState(false);
+  const [cardNumber, setCardNumber] = useState('');
+  const [bankAccountNumber, setBankAccountNumber] = useState('');
+  const [address, setAddress] = useState('');
+
+  const handleCheckoutClick = () => {
+    setShowCheckoutModal(true);
+  };
+
+  const handleCheckoutSubmit = () => {
+    if (!cardNumber || !bankAccountNumber || !address) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Please fill in all the fields!',
+      });
+      return;
+    }
+
+    // Handle submission logic here
+    // You can collect input data from the user and perform necessary actions
+    // For this example, let's just show a success alert
+    Swal.fire({
+      icon: 'success',
+      title: 'Checkout Successful!',
+      text: 'Thank you for your purchase!',
+    });
+
+    // Reset cart after successful checkout
+    setCartItems([]);
+    setShowCheckoutModal(false);
+  };
+
+  const handleCloseModal = () => {
+    setShowCheckoutModal(false);
+  };
+
   return (
     <div className='cartContainer'>
       <h2>Your Cart</h2>
@@ -45,7 +84,41 @@ const Cart = ({ cartItems, setCartItems }) => {
           </div>
         ))}
       </div>
-      <button className='checkoutButton'>Checkout</button>
+      
+      <button className='checkoutButton' onClick={handleCheckoutClick}>
+        Checkout
+      </button>
+
+      {/* Checkout Modal */}
+      {showCheckoutModal && (
+        <div className='checkoutModal'>
+          <div className='modalContent'>
+            <h3>Checkout</h3>
+            <input
+              type='text'
+              placeholder='Card Number'
+              value={cardNumber}
+              onChange={(e) => setCardNumber(e.target.value)}
+            />
+            <input
+              type='text'
+              placeholder='Bank Account Number'
+              value={bankAccountNumber}
+              onChange={(e) => setBankAccountNumber(e.target.value)}
+            />
+            <input
+              type='text'
+              placeholder='Address'
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+            <div className='modalButtons'>
+              <button onClick={handleCheckoutSubmit}>Submit</button>
+              <button onClick={handleCloseModal}>Cancel</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
