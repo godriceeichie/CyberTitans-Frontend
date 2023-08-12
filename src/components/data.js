83a27afe9ff90e7544216c39ec5b4c857392f8d5
just4tunes <fortuneosuh04@gmail.com>
2023-08-12 23:43:37 +0100

    updated FAQ

--- src/components/data.js
+++ src/components/data.js
@@ -1,31 +1,27 @@
 export const data = [ 
     {
-        question: "How can i pay for my order",
+        question: "How do I create an account on your website?",
         answer:
-        "Lorem ipsum dolor sit amet, consectetur."
+        "To create an account, click on the 'Sign Up' button at the top-right corner of the page. Fill in your details, such as your name, email address, and password, and then click 'Create Account'. You'll receive a confirmation email to verify your account."
     },
     {
-        question: "What is amazon pay",
+        question: "What payment methods do you accept?",
         answer:
-        "Lorem ipsum dolor sit amet, consectetur."
+        "We accept a variety of payment methods, including credit and debit cards (Visa, MasterCard, American Express), PayPal, and Apple Pay. You can choose your preferred method during the checkout process."
     },
     {
-        question: "I want to change my account information",
+        question: " How can I track my order?",
         answer:
-        "Lorem ipsum dolor sit amet, consectetur."
+        "Once your order is shipped, you'll receive a shipping confirmation email with a tracking number and a link to the courier's website. Simply click the link or enter the tracking number on their site to get real-time updates on the status of your delivery."
     },
     {
-        question: "How long does delevery take",
+        question: "Can I return or exchange an item?",
         answer:
-        "Lorem ipsum dolor sit amet, consectetur."
+        "Yes, we offer a hassle-free return and exchange policy. If you're not satisfied with your purchase, you can initiate a return within 30 days of receiving the item. Please review our Returns & Exchanges page for detailed instructions on how to proceed."
     },
     {
-        question: "How long does delevery take",
+        question: " How can I contact your customer support?",
         answer:
-        "Lorem ipsum dolor sit amet, consectetur."
+        " Our customer support team is here to help! You can reach us by clicking the 'Contact Us' link in the footer of the website. Alternatively, you can send an email to support@yourwebsite.com or call our toll-free customer service hotline at 1-800-123-4567 during our business hours, Monday to Friday, 9 AM to 6 PM."
     },
-
-
-
-
 ]
