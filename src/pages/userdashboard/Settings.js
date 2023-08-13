27420fadd15a3bf380f4b8dbca024b0c501b895f
BLACKHENZ29 <ayehenz29@gmail.com>
2023-08-13 03:34:58 +0100

    dash

--- src/pages/userdashboard/Settings.js
+++ src/pages/userdashboard/Settings.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import Usersidebar from './../../components/Usersidebar';
+import "../../styles/setting.css"
 
 const Settings = () => {
   return (
-    <div>
-      <h1>Settings</h1>
-    </div>
+    <div className="accountwrp">
+				<Usersidebar   />
+				<div className="settmenu">
+            <div className='set-con'>
+              <div className='set-nav'>
+                <button>Edit Profile</button>
+                <button>Shipping</button>
+                <button>Security</button>
+              </div>
+              <div className='edit-prf-bx'>
+                <div className='prf-img'>
+                  <img src={"../../assets/aloe-vera-img.webp"} alt="" />
+                </div>
+                <div className='prf-frm'>
+                  <form>
+                    <input type="text" placeholder='Username'/>
+                    <input type="text" placeholder='Name'/>
+                    <input type="text" placeholder='Email'/>
+                    <input type="text" placeholder='D.O.B'/>
+                    <input type="text" placeholder='Address'/>
+                    <button>Edit Profile</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
   );
 };
 
--- src/pages/userdashboard/Wishlist.js
+++ src/pages/userdashboard/Wishlist.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React from "react";
+import Usersidebar from "../../components/Usersidebar";
+import {AiOutlineClose} from "react-icons/ai"
+import "../../styles/wishlist.css"
 
 const Wishlist = () => {
   return (
-    <div>
-      <h1>Wishlist</h1>
+    <div className="accountwrp">
+      <Usersidebar />
+      <div className="wishmenu">
+        <p>My WishList</p>
+        <div className="wish-con">
+          <div className="wishbx">
+            <div className="img-hld"></div>
+            <div className="txt-hld">
+              <p className="itm-nm">Royal Cactus Hibiscus</p>
+              <p className="itm-dsc">This is a plant of which has many various things about it and so on and so forth there are more descriptions to be added to this but i no them not</p>
+            </div>
+            <div className="icn-hld">
+              <AiOutlineClose/>
+              <p className="itm-prc">$12.99</p>
+              <button className="atc">Add To Cart</button>
+            </div>
+          </div>
+          <div className="wishbx"></div>
+        </div>
+      </div>
     </div>
   );
 };
